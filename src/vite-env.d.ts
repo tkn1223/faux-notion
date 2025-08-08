479fc35c18915940e60677c907333664ffdaa6f7
tkn1223 <taka12230000@gmail.com>
2025-08-09 08:41:27 +0900

    supabaseの設定

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+// 環境変数そのものに対する型定義
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_API_KEY: string;
+}
+
+// 環境変数自体への型定義
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
