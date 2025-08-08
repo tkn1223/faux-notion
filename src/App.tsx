e0d88dc5e669870d36d22b791106ce5e3304e89b
tkn1223 <taka12230000@gmail.com>
2025-08-08 12:10:05 +0900

    作業途中：route作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import { Home } from "./pages/Home";
+import NoteDetail from "./pages/NoteDetail.tsx";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
-  )
+    <BrowserRouter>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/note/:id" element={<NoteDetail />} />
+          </Route>
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
