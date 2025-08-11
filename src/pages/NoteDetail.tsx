c7a955216aeceb5966c11dab8874590702aadd8a
tkn1223 <taka12230000@gmail.com>
2025-08-11 10:10:13 +0900

    ノートのtitelを表示させる

--- src/pages/NoteDetail.tsx
+++ src/pages/NoteDetail.tsx
@@ -1,3 +1,4 @@
+import { TitleInput } from "@/components/TitleInput";
 import { useCurrentUserStore } from "@/modules/auth/current-user.state";
 import { noteRepository } from "@/modules/notes/note.repository";
 import { useNoteStore } from "@/modules/notes/note.state";
@@ -31,7 +32,7 @@ const NoteDetail = () => {
   return (
     <div className="pb-40 pt-20">
       <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
-        {/* <TitleInput /> */}
+        <TitleInput initialData={note} />
       </div>
     </div>
   );
