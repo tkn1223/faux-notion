6b4f27c4633f21bd485f8f36ff3f3236cd2e9812
tkn1223 <taka12230000@gmail.com>
2025-08-11 10:01:28 +0900

    特定のIDに対するノートの情報を取得する処理

--- src/modules/notes/note.state.ts
+++ src/modules/notes/note.state.ts
@@ -4,24 +4,28 @@ import type { Note } from "./note.entity";
 const noteAtom = atom<Note[]>([]);
 
 export const useNoteStore = () => {
-    const [notes, setNotes] = useAtom(noteAtom);
+  const [notes, setNotes] = useAtom(noteAtom);
 
-    // setNotesを個別で作成する
-    // 従来：重複するデータを重複したまま保存する
-    // 新規：重複するデータを後からの情報で更新して保存する
-    const set = (newNotes: Note[]) => {
-        setNotes((oldNotes) => {
-            const combineNotes = [...oldNotes, ...newNotes];
+  // setNotesを個別で作成する
+  // 従来：重複するデータを重複したまま保存する
+  // 新規：重複するデータを後からの情報で更新して保存する
+  const set = (newNotes: Note[]) => {
+    setNotes((oldNotes) => {
+      const combineNotes = [...oldNotes, ...newNotes];
 
-            const uniqueNotes: { [key: number]: Note } = {};
-            for (const note of combineNotes) {
-                uniqueNotes[note.id] = note;
-            }
-            return Object.values(uniqueNotes);
-        });
-    };
-    return {
-        getAll: () => notes,
-        set,
-    };
+      const uniqueNotes: { [key: number]: Note } = {};
+      for (const note of combineNotes) {
+        uniqueNotes[note.id] = note;
+      }
+      return Object.values(uniqueNotes);
+    });
+  };
+
+  const getOne = (id: number) => notes.find((note) => note.id === id);
+
+  return {
+    getAll: () => notes,
+    getOne,
+    set,
+  };
 };
