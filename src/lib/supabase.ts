deccaff35b8dc633be9b2d4dce84657aacdba4f2
tkn1223 <taka12230000@gmail.com>
2025-08-11 22:58:49 +0900

    subscribeメソッドを実装

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,35 @@
-import { createClient } from "@supabase/supabase-js";
+import type { Note } from "@/modules/notes/note.entity";
+import {
+  createClient,
+  RealtimeChannel,
+  type RealtimePostgresChangesPayload,
+} from "@supabase/supabase-js";
 
 // Supabaseのクライアントを作成(初期化)
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_API_KEY
 );
+
+export const subscribe = (
+  userId: string,
+  callback: (payload: RealtimePostgresChangesPayload<Note>) => void
+) => {
+  return supabase
+    .channel("notes-changes")
+    .on<Note>(
+      "postgres_changes",
+      {
+        event: "*",
+        schema: "public",
+        table: "notes",
+        filter: `user_id=eq.${userId}`,
+      },
+      callback
+    )
+    .subscribe();
+};
+
+export const unsubscribe = (channel: RealtimeChannel) => {
+  supabase.removeChannel(channel);
+};
